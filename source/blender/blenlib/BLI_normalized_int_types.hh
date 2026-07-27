8890344a3f5b644954b0e6d560de0ab8481e98e0
RubensDesk <Rchiemstra.rh@gmail.com>
2026-07-27 19:33:46 +0200

    Refactor: Update NormalizedIntVec to use std::is_signed_v and add specialized implementations for int32_t and uint32_t

--- source/blender/blenlib/BLI_normalized_int_types.hh
+++ source/blender/blenlib/BLI_normalized_int_types.hh
@@ -18,12 +18,13 @@ template<typename T, int CompLen, int... CompBitCounts> struct NormalizedIntVec;
 template<typename T, int SizeX, int SizeY> struct NormalizedIntVec<T, 2, SizeX, SizeY> {
   using VecT = VecBase<float, 2>;
   using IntVecT = VecBase<T, 2>;
-  constexpr static bool is_signed = std::is_signed<T>();
-  constexpr static T x_max = (1 << (SizeX - int(is_signed))) - 1;
-  constexpr static T y_max = (1 << (SizeY - int(is_signed))) - 1;
 
   T x : SizeX;
   T y : SizeY;
+
+  constexpr static bool is_signed = std::is_signed_v<T>;
+  constexpr static T x_max = T((1u << (SizeX - int(is_signed))) - 1);
+  constexpr static T y_max = T((1u << (SizeY - int(is_signed))) - 1);
 
   NormalizedIntVec() = default;
   constexpr NormalizedIntVec(IntVecT value) : x(value.x), y(value.y) {}
@@ -51,16 +52,17 @@ template<typename T, int SizeX, int SizeY, int SizeZ, int SizeW>
 struct NormalizedIntVec<T, 4, SizeX, SizeY, SizeZ, SizeW> {
   using VecT = VecBase<float, 4>;
   using IntVecT = VecBase<T, 4>;
-  constexpr static bool is_signed = std::is_signed<T>();
-  constexpr static T x_max = (1 << (SizeX - int(is_signed))) - 1;
-  constexpr static T y_max = (1 << (SizeY - int(is_signed))) - 1;
-  constexpr static T z_max = (1 << (SizeZ - int(is_signed))) - 1;
-  constexpr static T w_max = (1 << (SizeW - int(is_signed))) - 1;
 
   T x : SizeX;
   T y : SizeY;
   T z : SizeZ;
   T w : SizeW;
+
+  constexpr static bool is_signed = std::is_signed_v<T>;
+  constexpr static T x_max = T((1u << (SizeX - int(is_signed))) - 1);
+  constexpr static T y_max = T((1u << (SizeY - int(is_signed))) - 1);
+  constexpr static T z_max = T((1u << (SizeZ - int(is_signed))) - 1);
+  constexpr static T w_max = T((1u << (SizeW - int(is_signed))) - 1);
 
   NormalizedIntVec() = default;
   constexpr NormalizedIntVec(IntVecT value) : x(value.x), y(value.y), z(value.z), w(value.w) {}
@@ -80,6 +82,76 @@ struct NormalizedIntVec<T, 4, SizeX, SizeY, SizeZ, SizeW> {
     if constexpr (is_signed) {
       return VecT(-x_max, -y_max, -z_max, -w_max);
     }
+    return VecT(0.0f, 0.0f, 0.0f, 0.0f);
+  }
+};
+
+template<>
+struct NormalizedIntVec<int32_t, 4, 10, 10, 10, 2> {
+  using VecT = VecBase<float, 4>;
+  using IntVecT = VecBase<int32_t, 4>;
+
+  int32_t x : 10;
+  int32_t y : 10;
+  int32_t z : 10;
+  int32_t w : 2;
+
+  constexpr static bool is_signed = true;
+  constexpr static int32_t x_max = 511;
+  constexpr static int32_t y_max = 511;
+  constexpr static int32_t z_max = 511;
+  constexpr static int32_t w_max = 1;
+
+  NormalizedIntVec() = default;
+  constexpr NormalizedIntVec(IntVecT value) : x(value.x), y(value.y), z(value.z), w(value.w) {}
+
+  operator IntVecT() const
+  {
+    return IntVecT(x, y, z, w);
+  }
+
+  static constexpr VecT max()
+  {
+    return VecT(511.0f, 511.0f, 511.0f, 1.0f);
+  }
+
+  static constexpr VecT min()
+  {
+    return VecT(-511.0f, -511.0f, -511.0f, -1.0f);
+  }
+};
+
+template<>
+struct NormalizedIntVec<uint32_t, 4, 10, 10, 10, 2> {
+  using VecT = VecBase<float, 4>;
+  using IntVecT = VecBase<uint32_t, 4>;
+
+  uint32_t x : 10;
+  uint32_t y : 10;
+  uint32_t z : 10;
+  uint32_t w : 2;
+
+  constexpr static bool is_signed = false;
+  constexpr static uint32_t x_max = 1023;
+  constexpr static uint32_t y_max = 1023;
+  constexpr static uint32_t z_max = 1023;
+  constexpr static uint32_t w_max = 3;
+
+  NormalizedIntVec() = default;
+  constexpr NormalizedIntVec(IntVecT value) : x(value.x), y(value.y), z(value.z), w(value.w) {}
+
+  operator IntVecT() const
+  {
+    return IntVecT(x, y, z, w);
+  }
+
+  static constexpr VecT max()
+  {
+    return VecT(1023.0f, 1023.0f, 1023.0f, 3.0f);
+  }
+
+  static constexpr VecT min()
+  {
     return VecT(0.0f, 0.0f, 0.0f, 0.0f);
   }
 };
